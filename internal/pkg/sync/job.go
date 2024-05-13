f95380a7e7a13ce6667de07cbda2b121677c501d
konstantin malikov <kostiamalikov@gmail.com>
2024-05-13 23:42:36 +0700

    Рефакторинг: разбиение на логические единицы сервиса accrual

--- internal/pkg/sync/job.go
+++ internal/pkg/sync/job.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/k0st1a/gophermart/internal/ports"
 	"github.com/rs/zerolog/log"
 )
@@ -40,7 +41,22 @@ func (j *job) Run(ctx context.Context) error {
 		return fmt.Errorf("storage error of get not processed order, error:%w", err)
 	}
 
+	accrual, err := j.getAccrual(ctx, tx, orderID)
+	if err != nil {
+		return err
+	}
+
+	err = j.updateBalance(ctx, tx, orderID, accrual)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (j *job) getAccrual(ctx context.Context, tx pgx.Tx, orderID int64) (*ports.Accrual, error) {
 	log.Printf("Job #%v, get accrual for orderID:%v", j.number, orderID)
+
 	orderString := strconv.FormatInt(orderID, 10)
 	accrual, err := j.client.Get(ctx, orderString)
 	if errors.Is(err, ports.ErrOrderNotRegistered) {
@@ -48,16 +64,18 @@ func (j *job) Run(ctx context.Context) error {
 
 		err = j.storage.UpdateOrder(ctx, tx, orderID, "INVALID", 0)
 		if err != nil {
-			return fmt.Errorf("storage error of update INVALID order, error:%w", err)
+			return nil, fmt.Errorf("storage error of update INVALID order, error:%w", err)
 		}
+
 		err = j.storage.Commit(ctx, tx)
 		if err != nil {
-			return fmt.Errorf("storage error of commit transaction, error:%w", err)
+			return nil, fmt.Errorf("storage error of commit transaction, error:%w", err)
 		}
-		return nil
+
+		return nil, fmt.Errorf("orderID:%v not registered in accrual", orderID)
 	}
 	if err != nil {
-		return fmt.Errorf("client error of get accrual for order, error:%w", err)
+		return nil, fmt.Errorf("client error of get accrual for order, error:%w", err)
 	}
 
 	log.Printf("Job #%v, for orderID:%v, accrual:%+v", j.number, orderID, accrual)
@@ -68,16 +86,23 @@ func (j *job) Run(ctx context.Context) error {
 
 		err = j.storage.UpdateOrder(ctx, tx, orderID, "INVALID", 0)
 		if err != nil {
-			return fmt.Errorf("storage error of update INVALID order, error:%w", err)
+			return nil, fmt.Errorf("storage error of update INVALID order, error:%w", err)
 		}
 
 		err = j.storage.Commit(ctx, tx)
 		if err != nil {
-			return fmt.Errorf("storage error of commit transaction, error:%w", err)
+			return nil, fmt.Errorf("storage error of commit transaction, error:%w", err)
 		}
-		return fmt.Errorf("other accrual order from response, order from request:%v"+
+
+		return nil, fmt.Errorf("other accrual order from response, order from request:%v"+
 			", order from response:%v", orderString, accrual.Order)
 	}
+
+	return accrual, nil
+}
+
+func (j *job) updateBalance(ctx context.Context, tx pgx.Tx, orderID int64, ar *ports.Accrual) error {
+	log.Printf("Job #%v, update balance for orderID:%v", j.number, orderID)
 
 	userID, err := j.storage.GetUserIDByOrderWithBlock(ctx, tx, orderID)
 	if err != nil {
@@ -91,15 +116,15 @@ func (j *job) Run(ctx context.Context) error {
 	}
 	log.Printf("Job #%v, for userID:%v, balance:%v", j.number, orderID, balance)
 
-	err = j.storage.UpdateOrder(ctx, tx, orderID, accrual.Status, accrual.Accrual)
+	err = j.storage.UpdateOrder(ctx, tx, orderID, ar.Status, ar.Accrual)
 	if err != nil {
 		return fmt.Errorf("storage error of update order, error:%w", err)
 	}
 
-	if accrual.Accrual != 0 {
+	if ar.Accrual != 0 {
 		log.Printf("Job #%v, accrual not 0 => update balance, userID:%v, new balance:%v",
-			j.number, userID, balance+accrual.Accrual)
-		err = j.storage.UpdateBalance(ctx, tx, userID, balance+accrual.Accrual)
+			j.number, userID, balance+ar.Accrual)
+		err = j.storage.UpdateBalance(ctx, tx, userID, balance+ar.Accrual)
 		if err != nil {
 			return fmt.Errorf("storage error of update balance, error:%w", err)
 		}
